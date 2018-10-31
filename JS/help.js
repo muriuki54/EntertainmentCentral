5a5ec692d1ad0465091ab354848561d2faee6b26
Charles Muriuki <muriukicharles54@gmail.com>
2018-10-31 21:01:11 +0300

    commit

--- JS/help.js
+++
@@ -1,7 +0,0 @@
-let toggleBtn = document.querySelector('.toggleBtn'),
-    sidebar = document.querySelector('.sidebar');
-
-    //  TOGGLE MENU
-toggleBtn.addEventListener('click',() => {
-    sidebar.classList.toggle('active')
-})
--- JS/main.js
+++ JS/main.js
@@ -9,6 +9,9 @@ let toggleBtn = document.querySelector('.toggleBtn'),
     showSaved = document.querySelector('#showSaved'),
     savedDetails = document.querySelector('.savedDetails');
 
+// window.addEventListener('load',() => {
+//     console.log(toggleBtn)
+// })
 
 //HERO SECTION IMAGE SLIDER
 let i = 0; //Start point
@@ -39,10 +42,15 @@ function changeImg() {
 changeImg();
 
 
-//  TOGGLE MENU
-toggleBtn.addEventListener('click',() => {
-    sidebar.classList.toggle('active')
+ //  TOGGLE MENU
+    toggleBtn.addEventListener('click',() => {
+        sidebar.classList.toggle('active');
+    })
+sidebar.addEventListener('click',() => {
+    sidebar.classList.remove('active');
 })
+
+    
 
 //SEARCH FIELDS
 form.addEventListener('submit',searchQuery);
@@ -73,7 +81,8 @@ function searchQuery(e){
         }
 
     }).catch((err) => {
-        alert(err)
+        console.log(err);
+        alert('Sorry we could not find a match')
     })
 } else {
     alert('Please Enter a value');
@@ -99,6 +108,79 @@ function getMovie(id,title) {
     window.location = 'movie.html';
 }
 
+function showSingle() {
+    let singleMovie = JSON.parse(sessionStorage.getItem('movie'));
+    console.log(movieDetails)
+
+    axios({
+        method: 'get',
+        url: 'https://www.omdbapi.com/?i='+singleMovie.id+'&apikey=7b4fc861'
+    }).then((single) => {
+        movieDetails.innerHTML += `
+    <div class="singlePoster">
+    <img src="${single.data.Poster}">
+    </div>
+    <div class="singleDetails">
+    <h2>${single.data.Title}</h2>
+    <div class="singleCard">
+    <h2>Movie Details </h2>
+    <ul>
+    <li>Year Released: ${single.data.Year}</li>
+    <li>Genre: ${single.data.Genre}</li>
+    <li>Director: ${single.data.Director}</li>
+    <li>Actors: ${single.data.Actors}</li>
+    <li>Language: ${single.data.Language}</li>
+    <li>IMDb Rating: ${single.data.Rating}</li>
+    </ul>
+    </div>
+    </div>
+    `;
+
+    plot.innerHTML += `
+    <div class="single">
+    <div class="singlePlot">
+    <h3><u>Plot</u></h3>
+    <p>${single.data.Plot}</p>    
+    </div>
+    </div>
+    <div class="actionBtn">
+    <button class="btn" id="add" onclick = "addList()">Add To List</button>  
+    <a href="index.html" class="btn remove">Home</a>    
+    </div>
+    `;
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+    function addList(e) {
+        let saveMovie = JSON.parse(sessionStorage.getItem('movie'));
+
+        console.log(saveMovie);
+        if(localStorage.getItem('savedMovie') == null) {
+            let savedMovie = [];
+            savedMovie.push(saveMovie);
+            localStorage.setItem('savedMovie',JSON.stringify(savedMovie));
+
+        } else {
+            let savedMovie = JSON.parse(localStorage.getItem('savedMovie'));
+            savedMovie.push(saveMovie);
+            localStorage.setItem('savedMovie',JSON.stringify(savedMovie));
+        } 
+        addBtn =  document.getElementById('add');
+        addBtn.style.background = '#1ede9e';
+        addBtn.innerHTML = 'Added succesfully';
+        addBtn.style.color = '#fff';
+
+        // alreadySaved = JSON.parse(localStorage.getItem('savedMovie'));
+        // for(var i = 0; i < alreadySaved.length;i++){
+        //     if(alreadySaved[i] == JSON.parse(sessionStorage.getItem('movie'))){
+        //         addBtn.innerHTML= 'Already Saved';
+        //     }
+        // }
+    }
+
+
 function displaySaved () {
     let savedMovie = JSON.parse(localStorage.getItem('savedMovie'));
    
@@ -113,6 +195,7 @@ function displaySaved () {
             <div class="savedDetails" style="background-image:url(${data.data.Poster})">
             <div class="innerDetails">
             <h1 class="left">${data.data.Title}</h1>
+            <button class="btn left" onclick="viewMovie('${data.data.imdbID}','${data.data.Title}')">Check out</button>
             <button class="btn left" onclick="removeMovie('${data.data.imdbID}')">Remove</btn>
             </div>
             </div>
@@ -123,6 +206,24 @@ function displaySaved () {
         })
     }   
 }
+
+displaySaved();
+
+function viewMovie(id,title) {
+    if(sessionStorage.getItem('movie') !== null){
+        sessionStorage.removeItem('movie');
+    }
+
+    let movie = {
+        id: id,
+        title: title
+    }
+    sessionStorage.setItem('movie',JSON.stringify(movie));
+    window.location = 'movie.html';
+    console.log(movie);
+    showSingle();
+}
+
 function removeMovie(id){
     let savedMovie = JSON.parse(localStorage.getItem('savedMovie'));
     for(var i = 0; i < savedMovie.length; i++){
@@ -132,60 +233,7 @@ function removeMovie(id){
     }
 
     localStorage.setItem('savedMovie',JSON.stringify(savedMovie));
-    displaySaved();
+    showSaved.innerHTML = '';
+    return displaySaved();
 }
-
-
-
-// function savedQuery(id){
-//     window.location = 'movie.html';
-//     sessionStorage.removeItem('movie');
-//     sessionStorage.setItem('id',id);
-    
-
-//     axios({
-//         method: 'get',
-//         url: ' http://www.omdbapi.com/?i='+id+'&apikey=7b4fc861'
-//     }).then((single) => {
-//         movieDetails.innerHTML += `
-//     <div class="singlePoster">
-//     <img src="${single.data.Poster}">
-//     </div>
-//     <div class="singleDetails">
-//     <h2>${single.data.Title}</h2>
-//     <div class="singleCard">
-//     <h2>Movie Details </h2>
-//     <ul>
-//     <li>Year Released: ${single.data.Year}</li>
-//     <li>Genre: ${single.data.Genre}</li>
-//     <li>Director: ${single.data.Director}</li>
-//     <li>Actors: ${single.data.Actors}</li>
-//     <li>Language: ${single.data.Language}</li>
-//     <li>IMDb Rating: ${single.data.Rating}</li>
-//     </ul>
-//     </div>
-//     </div>
-//     `;
-
-//     plot.innerHTML += `
-//     <div class="single">
-//     <div class="singlePlot">
-//     <h3><u>Plot</u></h3>
-//     <p>${single.data.Plot}</p>    
-//     </div>
-//     </div>
-//     <div class="actionBtn">
-//     <button class="btn" onclick = "addList()">Add To List</button>   
-//     <button class="btn">Download Torrent</button>   
-//     <a href="index.html" class="btn">Go to search</a>    
-//     </div>
-//     `;
-//     }).catch((err) => {
-//         console.log(err)
-//     })
-// }
-
-
-
-
 
